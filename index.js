10e3178d77381e1d8b159c3a235db9de1b57ce9d
azizasm <azizasm@gmail.com>
2016-02-16 00:39:06 +0800

    add in packege for pcslite

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  
+    socket.on('READ_MYKAD', function (from, msg) {
+    console.log('SERVER_ONLY msg', from, ' saying ', msg);
+	socket.emit('MYKAD_PAGE', 'yada ya');
+  });
+  
 });
 
 http.listen(3000, function(){
